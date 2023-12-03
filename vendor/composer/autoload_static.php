0ffa2932362837411ce870d49ccbe0fbff0ae423
Shoaib <shoaibsarwar187@gmail.com>
2023-12-03 23:15:31 +0100

    bramus-router-install

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,6 +20,16 @@ class ComposerStaticInit69089534e29489c0ec34c4a140ec23bb
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'B' => 
+        array (
+            'Bramus' => 
+            array (
+                0 => __DIR__ . '/..' . '/bramus/router/src',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -29,6 +39,7 @@ class ComposerStaticInit69089534e29489c0ec34c4a140ec23bb
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit69089534e29489c0ec34c4a140ec23bb::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit69089534e29489c0ec34c4a140ec23bb::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit69089534e29489c0ec34c4a140ec23bb::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit69089534e29489c0ec34c4a140ec23bb::$classMap;
 
         }, null, ClassLoader::class);
